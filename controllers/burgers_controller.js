e1e91bffa92755ab77ba9b65f2b7b3b0ff6558fa
ong_osmond <ong.osmond@yahoo.com>
2020-08-05 11:50:40 +0800

    Added update, add and delete functions; fixed the folder structure.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,4 +10,52 @@ router.get("/burgers", function(request, response) {
     });
 });
 
+
+// Add a burger defaulted to devoured = false
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.name, false
+    ], function(result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+
+// Update the burger devoured status
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    burger.update({
+            devoured: req.body.devoured
+        },
+        condition,
+        function(result) {
+            if (result.changedRows === 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
+            res.status(200).end();
+
+        }
+    );
+});
+
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,34 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-    selectAll: function(cb) {
+
+    selectAll: function(callback) {
         orm.selectAll("burgers", function(res) {
+            callback(res);
+        });
+    },
+
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(objColVals, condition, callback) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            callback(res);
+        });
+    },
+
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
             cb(res);
         });
     }
+
+
+
 };
 
 // Export the database functions for the controller.
