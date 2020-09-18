42ff2d1390f69c309fdd9cf13058ca71c65e5e78
ong_osmond <ong.osmond@yahoo.com>
2020-09-18 22:53:05 +0800

    Added employees table with sorting function

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-insert into burgers (burger_name, devoured)
-values ("Cheese Burger", false), ("Chicken Burger", false), ("Quarter Pounder Burger", false), ("Good Burger with nothing on it", true)
-; 
-
-select * from burgers;
